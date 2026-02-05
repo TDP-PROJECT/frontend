f065ba293d638dbc1814a4e73af76a86ff53814d
nayeong.oh <onyy540@gmail.com>
2026-02-05 16:18:37 +0900

    feat:  우측패널 ui 구현

--- app/viewer/page.tsx
+++ app/viewer/page.tsx
@@ -1,10 +1,12 @@
-import ThreeViewer from "../../components/ThreeViewer";
+import RightPannel from "@/components/RightPannel/RightPannel";
+import ThreeViewer from "@/components/ThreeViewer";
 
 export default function ViewerPage() {
   return (
     <div className="flex w-screen h-screen">
       <div className="flex-1">
         <ThreeViewer />
+        <RightPannel />
       </div>
     </div>
   );
