bfeb997f1626d4495935c8012da7b9708992c653
nayeong.oh <onyy540@gmail.com>
2026-02-05 16:39:38 +0900

    fix: type index.d.ts  export  안하도록 수정

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,10 @@
-
-export interface IPart {
+interface IPart {
   name: string;
   image: string;
   title: string;
   status: string;
   descript: string;
 }
-export type TabType = "기계공학" | "생명공학" | "의공학";
 type TabType = "기계공학" | "생명공학" | "의공학";
 
 // 우측패널
