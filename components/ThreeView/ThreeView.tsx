b6156d936298c5adc1f90fb4443b447e561ee4cd
nas7062 <nas7062@naver.com>
2026-02-10 14:34:10 +0900

    fix : 클릭 시 selectBox 닫히게

--- components/ThreeView/ThreeView.tsx
+++ components/ThreeView/ThreeView.tsx
@@ -28,12 +28,20 @@ interface Props {
   user: IUser | null;
   modelIdx: number;
   model: IModelDetail | null;
+  setIsMenu: Dispatch<SetStateAction<boolean | null>>;
 }
 function getModelPath(modelIdx: number) {
   return MODEL_PATH_BY_IDX[modelIdx] ?? "/models/Engine5.glb";
 }
 
-export default function ThreeView({ setSelectedName, selectedName, user, modelIdx, model }: Props) {
+export default function ThreeView({
+  setSelectedName,
+  selectedName,
+  user,
+  modelIdx,
+  model,
+  setIsMenu
+}: Props) {
   const modelPath = useMemo(() => getModelPath(modelIdx), [modelIdx]);
   const [isMoveCamera, setIsMoveCamera] = useState(false);
   // 모델 분해 상태
@@ -305,6 +313,7 @@ export default function ThreeView({ setSelectedName, selectedName, user, modelId
             selectedName={selectedName}
             setSelectedName={(name) => {
               setSelectedName(name);
+              setIsMenu(false);
               requestAnimationFrame(() => postSnapshot());
             }}
             originalColors={originalColors}
